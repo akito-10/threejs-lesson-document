328a0add112466f8bc315dc2b7d7fb4456a9d0f7
github akito-10 <akt.fukuda@gmail.com>
2022-12-18 17:33:55 +0900

    ✨ 線を描画

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import animate from "./Init";
+import writeLine from "./Line";
 import WebGL from "./WebGL";
 
 if (WebGL.isWebGLAvailable()) {
   // Initiate function or other initializations here
   animate();
+  writeLine();
 } else {
   const warning = WebGL.getWebGLErrorMessage();
   document.getElementById("container")?.appendChild(warning);
